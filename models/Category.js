81bc658c0597c18334d1ace31845f85864b31173
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-15 20:27:06 +0000

    created models and seeded them

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "category",
+};
+
+Category.init(schema, options);
 
 module.exports = Category;
--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,51 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    validate: { isDecimal: true },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: { isNumeric: true },
+    defaultValue: 10,
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "category",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product",
+};
+
+Product.init(schema, options);
 
 module.exports = Product;
