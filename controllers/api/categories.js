2a2d2b60c57f0cb8398ece0647f0a8ee0460e5f2
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-15 21:37:21 +0000

    created controllers

---
+++ controllers/api/categories.js
@@ -0,0 +1,32 @@
+const getAllCategories = (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+};
+
+const getCategoryById = (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+};
+
+const createNewCategory = (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+};
+
+const updateNewCategory = (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+};
+
+const deleteCategory = (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+};
+
+module.exports = {
+  getAllCategories,
+  getCategoryById,
+  createNewCategory,
+  updateNewCategory,
+  deleteCategory,
+};
